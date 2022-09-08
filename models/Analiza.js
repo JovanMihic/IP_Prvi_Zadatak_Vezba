d23d48d11b9e09d6fc80ad11db279b5f46aac9a7
JovanMihic <jovannrt11718@gs.viser.edu.rs>
2022-09-08 14:50:12 +0200

    first commit

---
+++ models/Analiza.js
@@ -0,0 +1,9 @@
+export class Analiza{
+    constructor(naziv,donjaRef,gornjaRef,merenaVrednost,cena){
+        this.naziv = naziv;
+        this.donjaRef = donjaRef;
+        this.gornjaRef = gornjaRef;
+        this.merenaVrednost = merenaVrednost;
+        this.cena = cena;
+    }
+}
---
+++ models/Korisnik.js
@@ -0,0 +1,8 @@
+export class Korisnik {
+    constructor(id, ime, prezime, listAnaliza) {
+        this.id = id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.listAnaliza = listAnaliza;
+    }
+}
